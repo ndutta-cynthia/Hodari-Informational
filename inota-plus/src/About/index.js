44df54dc6e889c65a9eb929641a930784b7360d6
Mutava-Ndinda <ndindafmutava@gmail.com>
2024-06-18 09:29:42 +0300

    created landingpage/about

---
+++ inota-plus/src/About/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const About=()=>{
+    return(
+        <>
+        <div id='about'>
+            <h1 id='abouthead'>We Are InotaPlus</h1>
+            <p>We provide a credit scoring system that generates a credit score based on information such as regular utility payments, 
+                savings, farmer's milk collections, and other sources of income; higher scores denote a lower default risk.
+                 Lenders use this score to determine the risk of granting credit or approving loans, 
+                 which affects the conditions and interest rates of those loans.</p>
+        </div>
+        </>
+    )
+}
+export default About;
