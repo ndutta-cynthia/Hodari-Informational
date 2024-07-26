893548e9ecb0e5ebf4e31434596a7a4a41a745f3
ndutta-cynthia <nduttamaina@gmail.com>
2024-07-26 08:24:09 +0300

    made changes

---
+++ src/About/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const About=()=>{
+    return(
+        <>
+        <div id='about'>
+            <h1 id='about-heading'>We Are InotaPlus</h1>
+            <p id='pg'>Credit score is a critical factor that influences the ability of one to get approved for loans,
+                 credit cards, mortgages and other forms of credit, as well as the interest rates and terms they receive.
+                 We provide a credit scoring system that generates a credit score based on information such as regular utility payments,
+                 savings, farmers' milk collections and other sources of income, to assess their creditworthiness. </p>
+        </div>
+        </>
+    )
+}
+export default About;
